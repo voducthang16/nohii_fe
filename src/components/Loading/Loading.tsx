f8b9099b655774e2611f93e949ee1fa9eb8f2ebc
Vo Duc Thang <voducthang16@gmail.com>
2023-06-11 22:43:02 +0700

    update file name

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,6 +1,6 @@
 import { LoadingOutlined } from '@ant-design/icons'
 import Spin from 'antd/es/spin';
-import "./loading.css";
+import "./Loading.css";
 
 const Loading = () => {
 	return (<div className={"loading"}>
